8abf230202076174e279d208a18ffdbb8988676b
Chad Hinds <chadrickj8@gmail.com>
2020-08-14 14:43:16 -0400

    Type aliases

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
+// This is an example of using type aliases that you can reference elsewhere in your code
+type Combinable = number | string;
+type Descriptor = "as number" | "as text";
+
 // Example of a Union Types
 function combine(
-  input1: number | string,
-  input2: number | string,
+  input1: Combinable,
+  input2: Combinable,
   // You can combine union type and literal type
-  resultType: "as number" | "as text"
+  resultType: Descriptor
 ) {
   let result;
 
@@ -24,3 +28,9 @@ console.log(combinedAges);
 
 const combinedNames = combine("Nicolle", "Brittany", "as text");
 console.log(combinedAges);
+
+// Type aliases can be used to "create" your own types.You're not limited to storing union types though
+// You can also provide an alias to a (possibly complex) object type. For example:
+
+type User = { name: string; age: number };
+const u1: User = { name: "Max", age: 30 }; // this works!
