2c30fdc9d6c0fb9a501641771e1e06c136149eb5
Chad Hinds <chadrickj8@gmail.com>
2020-09-09 11:22:48 -0400

    Function types

--- app.ts
+++ app.ts
@@ -1,10 +1,16 @@
-// return types determine what type it should return
+// return types determine what type is return from a function
 function add(n1: number, n2: number) {
   return n1 + n2;
 }
-
+// if a function does not return any value then you can specify void as return type
 function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
 printResult(add(5, 12));
+
+// Function types example
+let combinedValues: (a: number, b: number) => number;
+
+combinedValues = add;
+console.log(combinedValues(12, 4));
