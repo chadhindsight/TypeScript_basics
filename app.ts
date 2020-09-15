2add3230de77a8d859897369fe88c1f98eba85b1
Chad Hinds <chadrickj8@gmail.com>
2020-09-15 16:15:10 -0400

    cbs

--- app.ts
+++ app.ts
@@ -7,10 +7,18 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
 printResult(add(5, 12));
 
-// Function types example
+// Function types are types that describe a function
 let combinedValues: (a: number, b: number) => number;
 
 combinedValues = add;
 console.log(combinedValues(12, 4));
+
+addAndHandle(10, 12, (num) => {
+  console.log(num);
+});
