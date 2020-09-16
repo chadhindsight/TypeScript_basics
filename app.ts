0f40580c1168feed78e10f7eb7e9bad03e8e81ff
Chad Hinds <chadrickj8@gmail.com>
2020-09-16 14:05:54 -0400

    any type of value is assignable to unknown

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
 let userInput: unknown;
+
+//Much like any, any value is assignable to unknown; however, unlike any,
+// you cannot access any properties on values with the type unknown, nor can you call/construct them.
+userInput = 5;
+userInput = "max";
+if (typeof userInput === "string") {
+  let userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+  // while (true) {}
+}
+
+generateError("An error occurred!", 500);
