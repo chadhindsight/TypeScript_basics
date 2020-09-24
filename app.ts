1ee6cd4331c720e217db0a1ddcb481126545383b
Chad Hinds <chadrickj8@gmail.com>
2020-09-24 09:51:11 -0400

    the unknown type

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
+//Much like any, any type of value is assignable to unknown; however, unlike any,
+// you cannot access any properties on values with the type unknown, nor can you call/construct them.
 let userInput: unknown;
 
-//Much like any, any value is assignable to unknown; however, unlike any,
-// you cannot access any properties on values with the type unknown, nor can you call/construct them.
 userInput = 5;
 userInput = "max";
 if (typeof userInput === "string") {
