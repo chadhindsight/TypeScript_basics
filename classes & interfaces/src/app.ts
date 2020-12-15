06ae956f10e690b8ee887de39ef4d8996ea884ff
Chad Hinds <chadrickj8@gmail.com>
2020-12-15 13:23:04 -0500

    Fix spacing

--- classes & interfaces/src/app.ts
+++ classes & interfaces/src/app.ts
@@ -1,4 +1,11 @@
 // Classes are syntax sugar for constructor functions
 class Department {
+    name: string;
 
+    constructor(n:string) {
+        this.name = n
+    }
 }
+
+//Create a new object based on the blueprint of Department class
+const accounting = new Department('Accounting')
