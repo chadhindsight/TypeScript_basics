03b03040ea5be0394f7a701ae7222f07260a725d
Chad Hinds <chadrickj8@gmail.com>
2020-12-15 13:26:39 -0500

    make method associated with this class

--- classes & interfaces/src/app.ts
+++ classes & interfaces/src/app.ts
@@ -5,7 +5,12 @@ class Department {
     constructor(n:string) {
         this.name = n
     }
+    // method associated with this class
+    describe() {
+        console.log('Department: ' + this.name)
+    }
 }
 
 //Create a new object based on the blueprint of Department class
-const accounting = new Department('Accounting')
+const accounting = new Department('Accounting!')
+accounting.describe()
