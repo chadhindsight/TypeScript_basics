8362fa6fa7fd33bdc102c5170421683c5abf652d
Chad Hinds <chadrickj8@gmail.com>
2020-12-16 10:10:21 -0500

    readonly and private keywords are exclusive to typescript

--- classes & interfaces/src/app.ts
+++ classes & interfaces/src/app.ts
@@ -1,16 +1,31 @@
 // Classes are syntax sugar for constructor functions
 class Department {
-    name: string;
+    // private readonly id: string
+    // private name: string;
+    // the private keyword makes employees only accesible within this specific Department class
+    private employees: string[] = []
 
-    constructor(n:string) {
-        this.name = n
+    constructor(private id: string, public n:string) {
     }
+
     // method associated with this class
     describe() {
         console.log('Department: ' + this.name)
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+    printEmployeeInfo() {
+        console.log(this.employees.length, this.employees )
+    }
 }
 
 //Create a new object based on the blueprint of Department class
-const accounting = new Department('Accounting!')
+const accounting = new Department( 'id', 'Accounting!')
 accounting.describe()
+accounting.addEmployee('Devin')
+
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
