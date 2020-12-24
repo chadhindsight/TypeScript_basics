bfc9af8d18cafbb198a6229c3f4cd1a19530b18b
Chad Hinds <chadrickj8@gmail.com>
2020-12-24 10:07:52 -0500

    set name string in Greetable inteface to readonly

--- classes & interfaces/src/app.ts
+++ classes & interfaces/src/app.ts
@@ -1,17 +1,45 @@
 // Interface describes the structure of an object
-interface Person {
-    name: string,
-    age: number
+// interface Person {
+//     name: string,
+//     age: number
 
-   greet(phrase: string): void 
+//    greet(phrase: string): void 
+// }
+// // The user1 variable will be based on the Person interface
+// let user1: Person;
+
+// user1 = {
+//     name: 'Max',
+//     age: 29,
+//     greet(phrase:string) {
+//         console.log(phrase + '' + this.name)
+//     }
+// }
+
+///// INTERFACES WITH CLASSES /////
+//A diff between a custom type and an interface is that interface is only used to describe objects custom can be used to other stuff
+interface Greetable {
+  readonly name: string;
+
+  greet(phrase: string): void;
+}
+// You can implement more than one interface, but you can only inherit from one class
+class Person implements Greetable {
+  name: string;
+  age = 30;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
+  }
 }
 
-let user1: Person;
+let user1: Greetable;
 
-user1 = {
-    name: 'Max',
-    age: 29,
-    greet(phrase:string) {
-        console.log(phrase + '' + this.name)
-    }
-}
+user1 = new Person('Max');
+// Greetable forces anything based on it to have a greet method
+user1.greet('Hi there - I am');
+console.log(user1);
