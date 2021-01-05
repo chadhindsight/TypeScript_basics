f0a6dcf3c78acd5680fc0d3ee511f922b0cfcb4c
Chad Hinds <chadrickj8@gmail.com>
2021-01-05 12:33:10 -0500

    explan typegaurds using Car class example

--- intersection-types/src/app.ts
+++ intersection-types/src/app.ts
@@ -29,3 +29,44 @@ function add(a:Combinable, b:Combinable) {
   }
   return a + b
 }
+
+type UnknownEmployee = Employee | Admin
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name)
+  if('privileges' in emp) console.log('Privileges: ' + emp.privileges)
+  if('startDate' in emp) console.log('startDate: ' + emp.startDate)
+}
+
+printEmployeeInformation({name: 'Devin', startDate: new Date()})
+
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo ...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
