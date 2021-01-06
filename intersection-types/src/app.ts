22590229b1a7102befc0bf71e8ee644308ba5409
Chad Hinds <chadrickj8@gmail.com>
2021-01-06 11:42:08 -0500

    comment on use of example with  function overloading and optional chaining

--- intersection-types/src/app.ts
+++ intersection-types/src/app.ts
@@ -9,6 +9,7 @@ type Employee = {
 };
 
 // interface ElevatedEmployee extends Employee, Admin {}
+
 type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
@@ -22,13 +23,33 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
-function add(a:Combinable, b:Combinable) {
-  //Typeguards help us to know which exact type we get and what to do with it
-  if(typeof a === 'string' || typeof b === 'string') {
-      return a.toString() + b.toString()
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
   }
-  return a + b
+  return a + b;
 }
+
+const result = add('Max', ' Schwarz');
+result.split(' ');
+
+const fetchedUserData = {
+  id: 'u1',
+  name: 'Max',
+  job: { title: 'CEO', description: 'My own company' }
+};
+
+console.log(fetchedUserData?.job?.title);
+
+const userInput = undefined;
+
+const storedData = userInput ?? 'DEFAULT';
+
+console.log(storedData);
 
 type UnknownEmployee = Employee | Admin
 
