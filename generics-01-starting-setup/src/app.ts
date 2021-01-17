b70c773dc5b683f5a989fd52688a31c3693c015f
Chad Hinds <chadrickj8@gmail.com>
2021-01-17 15:37:30 -0500

    Add a function called extractAndConvert that demonstrates generic classes

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -14,8 +14,61 @@
 
 // Creating a generic function. This tells typescript that objA & objB can be diff datatypes
 // For generic types you can set certain contstraints for what types are allowed
-function merge(objA: object, objB: object) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
-const mergedObj = merge({ name: 'Max' }, { age: 31 });
+
+interface Lengthy {
+    length: number
+}
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 console.log(mergedObj);
+
+function countAndDescribe<T extends Lengthy>(el: T): [T, string] {
+    // return a tuple
+    let descriptionText = 'Got no value'
+    if (el.length === 1) { descriptionText = 'Got 1 element' }
+
+    else if (el.length > 1) { descriptionText = `Got ${el.length} elements.` }
+    return [el, descriptionText]
+}
+
+// the 'keyof' constraints
+function extractAndConvert<T extends object, U extends keyof T>(
+    obj: T,
+    key: U
+) {
+    return 'Value: ' + obj[key];
+}
+
+extractAndConvert({ name: 'Max' }, 'name');
+
+// Generic Classes
+extractAndConvert({ name: 'Max' }, 'name');
+
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1); // -1
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
