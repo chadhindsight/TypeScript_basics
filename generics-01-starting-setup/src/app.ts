3cfc3013bab66547d40ef2f6f76cdd1407a99ef2
Chad Hinds <chadrickj8@gmail.com>
2021-01-17 11:31:02 -0500

    Add an example of creating your own generic functions

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -1,9 +1,20 @@
 // GENERICS: An array without generics could contain anything. 
 // An array with generics can describe the values that the array contains.
-const names: Array = [];
+// const names: Array<string> = [];
 
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('this is done!')
-    }, 2000);
-})
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('this is done!')
+//     }, 2000);
+// })
+
+// promise.then(data =>{
+//     //data.split(" ")
+// })
+
+// Creating a generic function. This tells typescript that objA & objB can be diff datatypes
+function merge(objA: object, objB: object) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: 'Max' }, { age: 31 });
+console.log(mergedObj);
