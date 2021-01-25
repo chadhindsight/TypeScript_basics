aed3e22a2c5539bce5e1a1f5747a24624904473b
Chad Hinds <chadrickj8@gmail.com>
2021-01-25 18:31:49 -0500

    use a custom type for the merge function

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -12,7 +12,7 @@
 //     //data.split(" ")
 // })
 
-// Creating a generic function. This tells typescript that objA & objB can be diff datatypes
+// Creating and using a generic function. This tells typescript that objA & objB can be diff datatypes
 // For generic types you can set certain contstraints for what types are allowed
 function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
